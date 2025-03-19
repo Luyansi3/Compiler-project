332b9261db0d0124a0abfa7de532e985693305ca
Luyansi3 <lucas.sanchez@insa-lyon.fr>
2025-03-19 10:03:06 +0100

    add testcases

--- compiler/Linearize.cpp
+++ compiler/Linearize.cpp
@@ -11,36 +11,15 @@ antlrcpp::Any Linearize::visitProg(ifccParser::ProgContext *ctx)
 antlrcpp::Any Linearize::visitReturn_stmt(ifccParser::Return_stmtContext *ctx)
 {
 
-    // if(ctx->rvalue()->VAR()){
-    //     int index = symbolTable[ctx->rvalue()->VAR()->getText()].index;
-    //     std::cout << "    movl "<< index <<"(%rbp), %eax\n" ;
-    // }
-    // else{
-    //     int retval = stoi(ctx->rvalue()->CONST()->getText());
-    //     std::cout << "    movl $"<<retval<<", %eax\n" ;
-    // }
     this->visit(ctx->expr());
-
     return 0;
 }
 
 
 antlrcpp::Any Linearize::visitAffectation(ifccParser::AffectationContext *ctx)
 {
-    // string varName = ctx->lvalue()->VAR()->getText();
-
-    // if(ctx->rvalue()->CONST()){
-    //     int retval = stoi(ctx->rvalue()->CONST()->getText());
-    //     std::cout << "    movl $"<<retval <<", " << symbolTable[varName].index <<"(%rbp)\n" ;
-    // }
-    // else{
-    //     string varName2 = ctx->rvalue()->VAR()->getText();
-    //     std::cout << "    movl "<< symbolTable[varName2].index<<"(%rbp), " << symbolTable[varName].index <<"(%rbp)\n" ;
-    // }
-
     this->visit(ctx->expr());
     this->visit(ctx->lvalue());
-
 
     return 0;
 }
@@ -60,6 +39,9 @@ antlrcpp::Any Linearize::visitExprConst(ifccParser::ExprConstContext *ctx){
     
     return 0;
 }
+
+
+
 
 antlrcpp::Any Linearize::visitLvalue(ifccParser::LvalueContext *ctx){
 
---
+++ tests/testfiles/test_unaire/2_test_opUn_var.c
@@ -0,0 +1,6 @@
+int main() {
+    int a = 12;
+    a = -a;
+    return a;
+ }
+ 
