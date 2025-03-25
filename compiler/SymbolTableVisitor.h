3c1eeac837b7e91c01a0069f04d7466c86ddfefe
Selim Ben Bouzid <benbouzid.selim@gmail.com>
2025-03-25 12:01:38 +0100

    commit degueulasse

--- compiler/SymbolTableVisitor.h
+++ compiler/SymbolTableVisitor.h
@@ -7,18 +7,26 @@ using namespace std;
 #include <string>
 
 // Structure to hold information about a variable
-typedef struct Flag
+typedef struct FlagVar
 {
     int index;
     bool used;
     bool affected;
-    int nombreParams;
 }Flag;
+
+typedef struct FlagFonction
+{
+    bool used;
+    bool declared;
+    int nombreParams;
+}
 
 // Class to visit and build the symbol table
 class SymbolTableVisitor : public ifccBaseVisitor {
     public:
-        unordered_map<string, Flag> symbolTable; // Symbol table to store variable information
+        unordered_map<string, unordered_map<string, Flag>> symbolTableVar; // Symbol table to store variable information
+        static unrodered_map<string, FlagFonction> symbolTableFonction; //Symbol Table to store the function. It is static because common to all Symbol Table.
+        unordered_map<string, Flag> currentSymbolTable;
 
         // Constructor to initialize the base visitor and index
         SymbolTableVisitor(): ifccBaseVisitor(), index(-4) { }
@@ -29,6 +37,7 @@ class SymbolTableVisitor : public ifccBaseVisitor {
         virtual antlrcpp::Any visitExprVar(ifccParser::ExprVarContext *ctx) override;
         virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override;
         virtual antlrcpp::Any visitCall(ifccParser::CallContext * ctx) override;
+        virtual antlrcpp::Any visitDecl_fonction(ifccParser::Decl_fonction *ctx) override;
     private:
         int index; // Index to keep track of variable positions
 };
