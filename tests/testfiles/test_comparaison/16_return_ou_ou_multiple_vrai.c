4c75b8cdf09fc19c2cb406d35256641159f31eef
remivCode <remi.vialleton@gmail.com>
2025-03-28 15:10:39 +0100

    correction de tests

---
+++ tests/testfiles/test_comparaison/16_return_ou_ou_multiple_vrai.c
@@ -0,0 +1,7 @@
+int main() {
+    int a = 1;
+    int b = 1;
+    int c = 0;
+    int d = 0;
+    return a || b || c || d;
+}
--- tests/testfiles/test_comparaison/9_sup_vrai.c
+++ tests/testfiles/test_comparaison/9_sup_vrai.c
@@ -1,5 +1,5 @@
 int main() {
     int a = 2;
     int b = 1;
-    return a < b;
+    return a > b;
 }
