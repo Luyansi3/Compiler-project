1eb82d6b53936d041d7f45700f2871e4d3aa782a
Luyansi3 <lucas.sanchez@insa-lyon.fr>
2025-04-08 10:58:00 +0200

    Opti des If et While et correction div par 0

---
+++ tests/testfiles/test_arithmetique/test_optimisation_muldiv_addsub/2_opti_if.c
@@ -0,0 +1,21 @@
+int main() 
+{ 
+    int c = 0;
+    if(10){
+        int a = 3;
+        return 4;
+    }
+    else if(0)
+    {
+        return 34;
+    }
+    else if(0)
+    {
+        return 123;
+    }
+    else{
+        return 23;
+    }
+    
+    return 0;
+}
